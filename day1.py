a128eaf997af3de541c224c952d1d276a1b0555c
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-07-06 13:26:51 +0700

    Add day 1

---
+++ day1.py
@@ -0,0 +1,18 @@
+
+
+x= "String type "
+x=5
+print(x);
+
+
+# Gán nhiều giá trị trên cùng 1 dòng 
+
+
+#  cách 1 : Trường hợp mỗi biến là 1 giá trị khác nhau 
+s,x,f = "vang",3,5.5
+
+# cách 2 : Trường hợp tất cả biến đều chung 1 giá trị
+print(s,f)
+
+a,b=4,3
+print(a,b)
