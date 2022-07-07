6fb746f2331f5fc7762fa20059a43804aa742c07
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-07-07 09:39:52 +0700

    Day 2

---
+++ Day2.py
@@ -0,0 +1,30 @@
+# String day 
+
+# Chuỗi có thể dùng '' hoặc "" đều được 
+S_name ="an";
+S_name='aan'
+
+
+print(S_name)
+
+
+# để có thể 
+S_two_line= """This string has multiple line
+in one sentences
+"""
+print(S_two_line)
+
+# Muốn hiển thị giá trị của biến ra màn hình consle
+#dùng để in giá trị của 1 biến hoặc 1 số 
+
+# Mặc định khi dùng hàm print ( ) với mỗi tham số 
+# cách nhau dấu phẩy , sẽ tự động cách ra 1 khoảng khi xuất ra clg
+print(42,45)
+
+print("a =",42);
+
+# Hoặc có thể tự custom lại khoảng trắng giữa các giá trị bằng 1 
+# kí hiệu nào đó hoặc bằng 1 chuỗi nào đó 
+
+print(45,342,sep="-")
+#45-342
--- day1.py
+++ day1.py
@@ -14,5 +14,8 @@ s,x,f = "vang",3,5.5
 # cách 2 : Trường hợp tất cả biến đều chung 1 giá trị
 print(s,f)
 
-a,b=4,3
-print(a,b)
+# a,b=4,3  không nên ghi kiểu này sẽ vướng lỗi unpack 
+
+# Nhập dữ liệu từ console
+name = input("Nhập tên")
+print(type (name))
