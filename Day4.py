c093857f60617232b07dea722dd0aff55ae5fcb1
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-07-12 23:06:34 +0700

    Complete String

---
+++ Day4.py
@@ -0,0 +1,25 @@
+# Trong python khai bao chuoi bằng cách dùng ' '
+# hoặc " " đều như nhau
+
+# khai báo 1 chuỗi gồm nhiều dòng khác nhau
+# bằng cách dùng """ """
+
+# các thao tác với chuỗi trên python
+# -> tương tác như mảng
+
+word = "Python"
+
+print(word[0])  # Lấy phần tử đầu tiên của chuỗi
+print(word[-1])  # Lấy chữ phần tử cuối cùng của chuỗi \
+
+# lấy 1 đoạn ( substring ) từ A -> B
+
+
+print(word[2:5])
+
+# hoặc lấy từ 1 ví trí nào đó A , từ đầu đến A hoặc A đến hết chuỗi
+print(word[:2] ) # các ký tự từ đầu đến vị trí thứ 2 (loại bỏ)
+
+print(word[4:]) # các ký tự từ vị trí thứ 4(lấy) đến hết
+
+print(word[-3:])
